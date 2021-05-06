2923d64788d3a0d47a97b8964f78bcb2417a7fb6
hwillians <66992569+hwillians@users.noreply.github.com>
2021-05-06 10:33:20 +0200

    class tools

--- View/Program.cs
+++ View/Program.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace View
+﻿namespace View
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Tools.Menu();
         }
     }
 }
---
+++ View/Tools.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using static System.Console;
+
+namespace View
+{
+    public static class Tools
+    {
+        public static void Menu()
+        {
+            int choix = -1;
+
+            WriteLine("*** Ménu Gestion des employés ***" +
+                "\n1.- Ajouter un Client" +
+                "\n2.- Afficher la liste des Clients" +
+                "\n1.- Ajouter une Location" +
+                "\n2.- Afficher la liste des Locations" +
+                "\n0.- Sortir");
+
+            while (choix != 0)
+            {
+                choix = GetIntConsole("\nQuelle action voulez vouz effectuer : ");
+
+                switch (choix)
+                {
+                    case 1:
+                        WriteLine("Afficher un client");
+                        break;
+
+                    case 2:
+                        WriteLine("Afficher la liste des Clients");
+                        break;
+                    case 3:
+                        WriteLine("Ajouter une Location");
+                        break;
+                    case 4:
+                        WriteLine("Afficher la liste des Locations");
+                        break;
+
+                    case 0: WriteLine("à bientôt..."); break;
+
+                    default: WriteLine("Action non reconnue..."); break;
+                }
+            }
+        }
+
+        public static string GetStringConsole(string messag = "Valeur ")
+        {
+            Write(messag);
+            var s = ReadLine();
+            while (String.IsNullOrEmpty(s))
+            {
+                Write("Le text ne peux etre vide : ");
+                s = ReadLine();
+            }
+            return s;
+        }
+
+        public static double GetDoubleConsole(string messag)
+        {
+            double valeur;
+            while (!double.TryParse(GetStringConsole(messag), out valeur))
+                Write("Verifiez votre saisie : ");
+
+            return valeur;
+        }
+
+        public static int GetIntConsole(string messag)
+        {
+            int valeur;
+            while (!int.TryParse(GetStringConsole(messag), out valeur))
+                Write("Verifiez votre saisie : ");
+
+            return valeur;
+        }
+    }
+}
