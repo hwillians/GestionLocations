1059a55ec3b6a685fb6c406830f7eb95e4b94182
hwillians <66992569+hwillians@users.noreply.github.com>
2021-05-06 10:18:35 +0200

    class client

---
+++ Models/Client.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Models
+{
+    public class Client
+    {
+        public int Id { get; set; }
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+        public DateTime DateNaissance { get; set; }
+        public string Adresse { get; set; }
+        public string CodePostal { get; set; }
+        public string Ville { get; set; }
+
+        public override string ToString() => $"{Id} - {Nom} {Prenom}";
+    }
+}
